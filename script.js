951247b545e55e85b123f6cd0c954082d7b4e206
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-22 16:50:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+var lname = document.getElementById("text");
+lname.innerText = `You are using ${navigator.appName} version ${navigator.appVersion}`
+document.body.append(lname);
